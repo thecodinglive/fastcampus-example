f4562a5a1d6f4ec28cf1e4c99ef5dac84b13b75d
sjyun <darkandark@gmail.com>
2021-04-06 18:31:09 +0900

    spring admin config add

--- fastcam-photoapp/build.gradle.kts
+++ fastcam-photoapp/build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 dependencies {
 	implementation(project(":fastcam-spring-utils"))
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -31,6 +32,7 @@ dependencies {
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation ("org.springframework.cloud:spring-cloud-gcp-starter-storage")
+	implementation ("de.codecentric:spring-boot-admin-starter-client:2.4.0")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
