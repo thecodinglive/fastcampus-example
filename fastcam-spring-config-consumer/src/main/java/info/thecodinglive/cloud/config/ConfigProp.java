de3a6d4516ce9ecaecddee204ce1d7a4530412e0
sjyun <darkandark@gmail.com>
2021-04-10 01:46:30 +0900

    config-consumer

--- fastcam-spring-config-consumer/src/main/java/info/thecodinglive/cloud/config/ConfigProp.java
+++ fastcam-spring-config-consumer/src/main/java/info/thecodinglive/cloud/config/ConfigProp.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
+
 @Data
 @ConfigurationProperties("mongodbserver")
 public class ConfigProp {
